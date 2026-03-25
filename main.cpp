77018723a548b1d04f02deeff0b52416e86be0d9
Aren <106656925+meltyblend@users.noreply.github.com>
2026-03-25 09:40:05 -0700

    deleted a file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    int A = 0;
+    int B = 1;
+
+    std::cout << A + B << std::endl;
+    std::cout << "Hello" << std::endl;
+
+    return 0;
+}
+
+/////////////////Forward computation
+
+
+
+////////////////Backward computation
